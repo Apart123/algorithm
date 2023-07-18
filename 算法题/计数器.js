359434a31505ecb89e5a6be64223ae3479d71155
luying <18280977332@163.com>
2023-07-18 20:52:40 +0800

    计数器

---
+++ 算法题/计数器.js
@@ -0,0 +1,47 @@
+/*请你编写并返回一个计数器函数，它接收一个整型参数 n 。这个计数器函数最初返回 n，每次调用它时返回前一个值加 1 的值 ( n ,n + 1 , n + 2 ，等等)。
+
+示例 1：
+输入：
+n = 10
+["call","call","call"]
+输出：[10,11,12]
+解释：
+counter() = 10 // 第一次调用 counter()，返回 n。
+counter() = 11 // 返回上次调用的值加 1。
+counter() = 12 // 返回上次调用的值加 1。
+
+示例 2：
+输入：
+n = -2
+["call","call","call","call","call"]
+输出：[-2,-1,0,1,2]
+解释：counter() 最初返回 -2。然后在每个后续调用后增加 1。*/
+
+/**
+ * @param {number} n
+ * @return {Function} counter
+ */
+var createCounter = function (n) {
+    return function () {
+        // return n++;
+        console.log(n++);
+    };
+};
+
+
+/*
+const counter = createCounter(10)
+counter() // 10
+counter() // 11
+counter() // 12
+*/
+
+/*
+const counter = createCounter(-2);
+counter();
+counter();
+counter();
+counter();
+counter();
+ */
+
